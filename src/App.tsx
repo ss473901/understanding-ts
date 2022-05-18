136cdf96fb66fb8b587f95cd70b279e319258c6a
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-05-18 22:56:28 +0900

    Add todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import NewTodo from "./components/NewTodo";
+import TodoList from "./components/TodoList";
+import { Todo } from "./todo.model";
 
-function App() {
-  return <div className="App"></div>;
-}
+// function App() {
+//   return <div className="App"></div>;
+// }
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const todoAddHandler = (text: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
+  return (
+    <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} />
+    </div>
+  );
+};
 
 export default App;
